bc71d0578a24e8cda5bb237e7f48a3800876fa77
Gods-Blessing <mohitadhana2569@gmail.com>
2023-01-11 20:16:23 +0530

    final commit

--- models/Product.js
+++ models/Product.js
@@ -20,6 +20,6 @@ const productSchema = new mongoose.Schema([{
 });
 
 
-const Product = mongoose.model("User", productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 module.exports = Product;
